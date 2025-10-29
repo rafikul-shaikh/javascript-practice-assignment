99d956939ac446677b34c2eed8252ce44a53f697
Rafikul shaikh <rafikulshaikh510@gmail.com>
2025-10-29 11:27:54 +0530

    basic q1 of assignment1

---
+++ assignment-1/1-basic-level/question1.js
@@ -0,0 +1,11 @@
+// Q.1) Create a function that uses setTimeout to simulate a 3-second delay before logging “Data fetched”.
+
+function fetchData() {
+  console.log("Fetching data...");
+
+  setTimeout(() => {
+    console.log("Data fetched");
+  }, 3000); // 3000 milliseconds = 3 seconds
+}
+
+fetchData();
